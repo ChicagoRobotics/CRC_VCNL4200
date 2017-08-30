5bec2c2f140c1c66ebed30925146c1b0062962b4
Jamie Laing <jamie@jamielaing.com>
2017-08-30 08:49:30 -0500

    VCNL 4200 I2C control library

--- CRC_VCNL4200/CRC_VCNL4200.ino
+++ CRC_VCNL4200/CRC_VCNL4200.ino
@@ -4,12 +4,21 @@
  Author:	jlaing
 */
 
+int ledPin = 13;
+int sdaPin = 20;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	pinMode(ledPin, OUTPUT);
+	pinMode(sdaPin, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	digitalWrite(ledPin, HIGH);   // sets the LED on
+	digitalWrite(sdaPin, HIGH);
+	delay(2000);                  // waits for a second
+	digitalWrite(ledPin, LOW);    // sets the LED off
+	digitalWrite(sdaPin, LOW);
+	delay(2000);
 }
