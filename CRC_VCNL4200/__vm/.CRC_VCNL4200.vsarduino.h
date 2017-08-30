a23d53af50c50754461959011879e13d5e0cef53
Jamie Laing <jamie@jamielaing.com>
2017-08-30 14:41:31 -0500

    updates caused by alternate desktop

--- CRC_VCNL4200/__vm/.CRC_VCNL4200.vsarduino.h
+++ CRC_VCNL4200/__vm/.CRC_VCNL4200.vsarduino.h
@@ -5,18 +5,18 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino/Genuino Mega w/ ATmega1280, Platform=avr, Package=arduino
+	Hardware: Arduino/Genuino Mega w/ ATmega2560 (Mega 2560), Platform=avr, Package=arduino
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega1280__
+#define __AVR_ATmega2560__
 #define _VMDEBUG 1
 #define F_CPU 16000000L
 #define ARDUINO 10609
-#define ARDUINO_AVR_MEGA
+#define ARDUINO_AVR_MEGA2560
 #define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
 #define __AVR__
@@ -62,7 +62,7 @@ typedef void *__builtin_va_list;
 
 
 
-#include <Arduino.h>
+#include <arduino.h>
 #include <pins_arduino.h> 
 #undef F
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
